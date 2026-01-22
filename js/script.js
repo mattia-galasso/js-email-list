20dd6f84a21ad81f01704bb3a3e2976042a8ebde
MattiaGalasso <galasso.mattia94@gmail.com>
2026-01-22 15:16:42 +0100

    Added Axios API Request, Added Email Generation and Added Button For New Gen

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("JS Ready");
+//? DOM ELEMENTS
+const emailList = document.getElementById("email-list");
+const newGenButton = document.getElementById("new-gen-button");
+
+//? GENERAL VARIABLES
+const emailNumber = 10;
+
+newGenButton.addEventListener("click", generateMail);
+
+function generateMail(whoManyEmail) {
+  emailList.innerHTML = "";
+
+  for (let i = 0; i < emailNumber; i++) {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((res) => {
+        const randomEmail = res.data.response;
+        emailList.innerHTML += `
+            <li class="email-item">
+              <a href="mailto:${randomEmail}">${randomEmail}</a>
+            </li>
+          `;
+      });
+  }
+}
